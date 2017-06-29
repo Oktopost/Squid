8dd4458140290d0578b1b5321eb37b22c7422731
Alexey Puchkov <alexey@oktopost.com>
2017-06-29 10:11:42 +0300

    Remove unnessesery interfaces from IGenericObjectConnector

--- src/Squid/MySql/Connectors/Object/Generic/IGenericObjectConnector.php
+++ src/Squid/MySql/Connectors/Object/Generic/IGenericObjectConnector.php
@@ -4,10 +4,6 @@ namespace Squid\MySql\Connectors\Object\Generic;
 
 use Squid\MySql\Connectors\Generic\ICountConnector;
 use Squid\MySql\Connectors\Generic\IDeleteConnector;
-use Squid\MySql\Connectors\Generic\IInsertConnector;
-use Squid\MySql\Connectors\Generic\ISelectConnector;
-use Squid\MySql\Connectors\Generic\IUpdateConnector;
-use Squid\MySql\Connectors\Generic\IUpsertConnector;
 
 use Squid\MySql\Connectors\Object\IPlainObjectConnector;
 
@@ -16,11 +12,7 @@ interface IGenericObjectConnector extends
 	IPlainObjectConnector,
 	
 	ICountConnector,
-	IDeleteConnector,
-	IUpdateConnector,
-	IUpsertConnector,
-	ISelectConnector,
-	IInsertConnector
+	IDeleteConnector
 {
 
 }

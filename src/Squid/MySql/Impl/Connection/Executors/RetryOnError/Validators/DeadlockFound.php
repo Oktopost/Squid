4374d49d3756b977c6af40d706923d7b454733b8
Daniel Podrabinek <dpodrabinek@oktopost.com>
2018-02-27 12:59:50 +0200

    Updated DeadLockFound error validator

--- src/Squid/MySql/Impl/Connection/Executors/RetryOnError/Validators/DeadlockFound.php
+++ src/Squid/MySql/Impl/Connection/Executors/RetryOnError/Validators/DeadlockFound.php
@@ -2,11 +2,15 @@
 namespace Squid\MySql\Impl\Connection\Executors\RetryOnError\Validators;
 
 
+use Squid\MySql\Exceptions\MySqlException;
 use Squid\MySql\Impl\Connection\Executors\RetryOnError\Base\AbstractErrorValidator;
 
 
 class DeadlockFound extends AbstractErrorValidator
 {
+	private const DEAD_LOCK_CODE = 1213;
+	
+	
 	/**
 	 * @param int $ms Number of ms to wait before trying again
 	 * @param int $retries Maximum number of retries
@@ -23,8 +27,8 @@ class DeadlockFound extends AbstractErrorValidator
 	 */
 	public function isHandled(\Exception $e)
 	{
-		return $this->isMessageMatch(
-			$e, 
-			"/^Mysqli statement execute error : Deadlock found when trying to get lock; try restarting transaction$/"); 
+		return
+			$e instanceof MySqlException &&
+			$e->getCode() == self::DEAD_LOCK_CODE;
 	}
 }

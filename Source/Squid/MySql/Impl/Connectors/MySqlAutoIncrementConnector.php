4a05bba1ca0d44d15afeb4dd587cf600be2ecfe0
Alexey Puchkov <alexey.pkv@gmail.com>
2023-06-04 15:05:41 +0300

    Add some deprecated connectors for historical support

---
+++ Source/Squid/MySql/Impl/Connectors/MySqlAutoIncrementConnector.php
@@ -0,0 +1,104 @@
+<?php
+namespace Squid\MySql\Impl\Connectors;
+
+
+use Squid\MySql\Connectors\IMySqlAutoIncrementConnector;
+
+use Objection\LiteObject;
+
+
+/**
+ * @deprecated 
+ */
+class MySqlAutoIncrementConnector extends MySqlObjectConnector implements IMySqlAutoIncrementConnector
+{
+	private $idField = 'Id';
+	private $idFieldArray = ['Id'];
+	
+	
+	/**
+	 * @param LiteObject $object
+	 * @return bool
+	 */
+	private function insertNewObject(LiteObject $object)
+	{
+		$this->addIgnoreFields([$this->idField]);
+		
+		if (!$this->insert($object))
+			return false;
+		
+		$lastId = $this->getConnector()->controller()->lastId();
+		
+		if ($lastId !== false)
+		{
+			$object->{$this->idField} = $lastId;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	/**
+	 * @param string $field
+	 * @return static
+	 */
+	public function setIdField($field = 'Id')
+	{
+		$this->idField = $field;
+		$this->idFieldArray = [$field];
+		return $this;
+	}
+	
+	/**
+	 * @param LiteObject $object
+	 * @return bool
+	 */
+	public function save(LiteObject $object)
+	{
+		if (is_null($object->{$this->idField}))
+		{
+			return $this->insertNewObject($object);
+		}
+		else
+		{
+			return $this->update($object);
+		}
+	}
+	
+	/**
+	 * @param LiteObject $object
+	 * @return bool
+	 */
+	public function update(LiteObject $object)
+	{
+		$data = $this->objectsToData([$object])[0];
+		
+		foreach ($this->idFieldArray as $idField)
+		{
+			unset($data[$idField]);
+		}
+		
+		return $this->updateByFields(
+			$data, 
+			[ $this->idField => $object->{$this->idField} ]);
+	}
+	
+	/**
+	 * @param int $id
+	 * @return LiteObject|null
+	 */
+	public function load($id)
+	{
+		return $this->loadOneByField($this->idField, $id);
+	}
+	
+	/**
+	 * @param int $id
+	 * @return bool
+	 */
+	public function delete($id)
+	{
+		return $this->deleteByField($this->idField, $id);
+	}
+}

af0bb39769dbab0f3c261e96b8a5b90835d580f1
Daniel Podrabinek <dpodrabinek@oktopost.com>
2025-05-22 21:02:59 +0300

    Updated default migration

--- Tests/MySQL/create.sql
+++ Tests/MySQL/create.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS _squid_test_;
 
 CREATE USER IF NOT EXISTS '_squid_test_u_'@'%' IDENTIFIED WITH mysql_native_password BY '_squid_test_pass_';
-GRANT ALL PRIVILEGES ON `_squid_test_`.* TO '_squid_test_u_'@'%';
+GRANT ALL PRIVILEGES ON _squid_test_.* TO '_squid_test_u_'@'%';
 FLUSH PRIVILEGES;

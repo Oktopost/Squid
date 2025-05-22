bf9072a03da53a01d13dbee2f3905b992bcbc095
Daniel Podrabinek <dpodrabinek@oktopost.com>
2025-05-22 20:47:25 +0300

    Updated configuration for github action

--- Tests/MySQL/create.sql
+++ Tests/MySQL/create.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE _squid_test_;
+CREATE DATABASE IF NOT EXISTS _squid_test_;
 USE _squid_test_;
 
-
-CREATE USER '_squid_test_u_'@'%' IDENTIFIED BY '_squid_test_pass_';
+CREATE USER IF NOT EXISTS '_squid_test_u_'@'%' IDENTIFIED WITH mysql_native_password BY '_squid_test_pass_';
 GRANT ALL PRIVILEGES ON `_squid_test_`.* TO '_squid_test_u_'@'%';
+FLUSH PRIVILEGES;
